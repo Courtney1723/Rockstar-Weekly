de3ecf5de80cdba33d559a04c9c120c341dcfb73
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2022-06-01 13:43:05 -0600

    updated formatting

--- commands/RDO.js
+++ commands/RDO.js
@@ -26,9 +26,6 @@ module.exports = {
     let rdoMonthBody = args[4].split("\\u003c\/li\\u003e\\u003c\/")
 
       let rdoMonthBody1 = rdoMonthBody[0].split("{\"html\":\"\\u003cul\\u003e\\u003cli\\u003e \\u003cb\\u003e") //gets the latest Monthly update 
-
-
-
     
     let rdoLaterBody = args[6].split("u003cb\\u003e")  
 
@@ -38,7 +35,7 @@ module.exports = {
 
         let rdoLaterBody1 = args[6].split("\\u003cul\\u003e\\u003cli\\u003e \\u003cb\\u003e") 
 
-        let rdoLaterBody2 = rdoLaterBody1[1].split("\\u003c\/li\\u003e\\u003c\/ul\\u003e\"") // gets the later update body post
+        let rdoLaterBody2 = rdoLaterBody1[1].split("\\u003c\/li\\u003e\\u003c\/ul\\u003e\"") // gets the "other" update body post
     
 
 
@@ -56,13 +53,16 @@ module.exports = {
     message.channel.send({embed: {
             color: 0xB75AFF, //Purple
             title: `Red Dead Redemption II Online Weekly Bonuses & Discounts:`,
-            description: `**Last updated: ${rdoDate[0]}**\n\n${rdoBody1[1]}\n\n**Monthly Bonuses:**\n${rdoMonthBody1[1]} \n\n **Later Bonuses:**\n ${rdoLaterBodyDate1[0]}\n\n${rdoLaterBody2[0]}\n\n[Click Here](https://www.ign.com/wikis/red-dead-redemption-2/Red_Dead_Online_Updates_Archive) to view more bonuses and discounts`
+            description: `**Last Updated ${rdoDate[0]}**\n\n${rdoBody1[1]}\n\n**Monthly Bonuses:**\n${rdoMonthBody1[1]} \n\n **Other Bonuses:**\n ${rdoLaterBodyDate1[0]}\n\n${rdoLaterBody2[0]}\n\n[Click Here](https://www.ign.com/wikis/red-dead-redemption-2/Red_Dead_Online_Updates_Archive) to view more bonuses & discounts`
             .replace(/\\u0026#x2019;/g , "'")
             .replace(/\\u003c\/b\\u003e\\u003cbr\\u003e/g, "\n")
             .replace(/\\u003cbr\\u003e/g, "\n")
             .replace(/\\u003c\/li\\u003e\\n\\u003cli\\u003e \\u003cb\\u003e/g, "\n\n")
-            .replace(/\\u0026#x2018;/g, "'")
-
+            .replace(/\\u0026#x2018;/g, "'")     
+            .replace(/\\u003c\/li\\u003e\\n\\u003cli\\u003e\\u003cb\\u003e/g, "\n")
+            .replace(/\\u0026#x201C;/g, "\n")
+            .replace(/\\u0026#x201D;/g, "")
+            .replace(/Prime Gaming accounts by visiting \\u003ca rel=\\\"nofollow\\\" class=\\\"external text\\\" href=\\\"https:\/\/twitch.amazon.com\/RDO\\\"\\u003eTwitch.amazon.com\/RDO\\u003c\/a\\u003e.\\u003c\/b\\u003e/g, "Prime Gaming accounts by [clicking here](https://twitch.amazon.com/RDO)")
                                     
                         
                         
