ef9957854b4874b18e6af9d390a0f2ed80f47784
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2022-06-01 17:37:16 -0600

    updated to discord.js v13

--- commands/RDO.js
+++ commands/RDO.js
@@ -1,17 +1,18 @@
-const fs = require("fs")
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const fetch = require("node-fetch") //Required for .fetch() 
+const { MessageEmbed } = require('discord.js');
 
-const prefix = "?"; //Creates a prefix $
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 
 
 module.exports = {
-	name: 'rdo',
-	description: 'rdo command',
-	async execute(message, user) {
+	data: new SlashCommandBuilder()
+		.setName('rdo')
+		.setDescription('Replies with RDO latest bonuses and discounts'),
+async execute(interaction, user) {
 
-  if (!message.content.toLowerCase().startsWith(prefix + "rdo")) return;
+
 
   const rdo1 =   await fetch('https://www.ign.com/wikis/red-dead-redemption-2/Red_Dead_Online_Updates_Archive#Red_Dead_Redemption_2_Online_Updates').then(res => res.text())
 
@@ -26,6 +27,9 @@ module.exports = {
     let rdoMonthBody = args[4].split("\\u003c\/li\\u003e\\u003c\/")
 
       let rdoMonthBody1 = rdoMonthBody[0].split("{\"html\":\"\\u003cul\\u003e\\u003cli\\u003e \\u003cb\\u003e") //gets the latest Monthly update 
+
+
+
     
     let rdoLaterBody = args[6].split("u003cb\\u003e")  
 
@@ -35,7 +39,7 @@ module.exports = {
 
         let rdoLaterBody1 = args[6].split("\\u003cul\\u003e\\u003cli\\u003e \\u003cb\\u003e") 
 
-        let rdoLaterBody2 = rdoLaterBody1[1].split("\\u003c\/li\\u003e\\u003c\/ul\\u003e\"") // gets the "other" update body post
+        let rdoLaterBody2 = rdoLaterBody1[1].split("\\u003c\/li\\u003e\\u003c\/ul\\u003e\"") // gets the later update body post
     
 
 
@@ -45,15 +49,12 @@ module.exports = {
 
   let rdoDate = args[3].split(", 2022") //Gets the Date
 
+  let rdoEmbed = new MessageEmbed()
+      .setColor('#B75AFF') //Purple
+      .setTitle('Red Dead Redemption II Online Weekly Bonuses & Discounts:')
+      .setDescription(`**Last Updated ${rdoDate[0]}**\n\n${rdoBody1[1]}\n\n**Monthly Bonuses:**\n${rdoMonthBody1[1]} \n\n **Other Bonuses:**\n ${rdoLaterBodyDate1[0]}\n\n${rdoLaterBody2[0]}\n\n[Click Here](https://www.ign.com/wikis/red-dead-redemption-2/Red_Dead_Online_Updates_Archive) to view more bonuses and discounts`
 
-
-  if (message.content.toLowerCase().startsWith(prefix + "rdo")) {
-
-  
-    message.channel.send({embed: {
-            color: 0xB75AFF, //Purple
-            title: `Red Dead Redemption II Online Weekly Bonuses & Discounts:`,
-            description: `**Last Updated ${rdoDate[0]}**\n\n${rdoBody1[1]}\n\n**Monthly Bonuses:**\n${rdoMonthBody1[1]} \n\n **Other Bonuses:**\n ${rdoLaterBodyDate1[0]}\n\n${rdoLaterBody2[0]}\n\n[Click Here](https://www.ign.com/wikis/red-dead-redemption-2/Red_Dead_Online_Updates_Archive) to view more bonuses & discounts`
+                      
             .replace(/\\u0026#x2019;/g , "'")
             .replace(/\\u003c\/b\\u003e\\u003cbr\\u003e/g, "\n")
             .replace(/\\u003cbr\\u003e/g, "\n")
@@ -62,18 +63,27 @@ module.exports = {
             .replace(/\\u003c\/li\\u003e\\n\\u003cli\\u003e\\u003cb\\u003e/g, "\n")
             .replace(/\\u0026#x201C;/g, "\n")
             .replace(/\\u0026#x201D;/g, "")
-            .replace(/Prime Gaming accounts by visiting \\u003ca rel=\\\"nofollow\\\" class=\\\"external text\\\" href=\\\"https:\/\/twitch.amazon.com\/RDO\\\"\\u003eTwitch.amazon.com\/RDO\\u003c\/a\\u003e.\\u003c\/b\\u003e/g, "Prime Gaming accounts by [clicking here](https://twitch.amazon.com/RDO)")
+            .replace(/Prime Gaming accounts by visiting \\u003ca rel=\\\"nofollow\\\" class=\\\"external text\\\" href=\\\"https:\/\/twitch.amazon.com\/RDO\\\"\\u003eTwitch.amazon.com\/RDO\\u003c\/a\\u003e.\\u003c\/b\\u003e/g, "Prime Gaming accounts by [clicking here](https://twitch.amazon.com/RDO)")                     
+                     
+                     
+                     )
+
+    
+
+
+  
+    return interaction.reply({ embeds: [rdoEmbed] })
+
                                     
                         
                         
                         
                         
-                        }})
+                        }}
     
     
 
 
 
-  }}}
           
 
