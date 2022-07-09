769dd717f9a1025a168bd828f687b73bd32cb4f8
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2022-07-09 11:48:27 -0600

    updated to add new "code" and "vote" commands

--- commands/help.js
+++ commands/help.js
@@ -1,19 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
-		.setDescription('Replies with all the TestBot interactions'),
+		.setDescription('Replies with all the Rockstar Weekly interactions'),
 	async execute(interaction) {
 
-
       let helpEmbed = new MessageEmbed()
-      .setColor('#B75AFF') //Purple
+      .setColor('#00FFFF') //Teal
       .setTitle(`Rockstar Weekly Bot Commands`)
-      .setDescription(`**/help**\n> provides a list of commands \n**/ping**\n> The Rockstar Weekly bot responds with pong! when online \n**/rdo**\n> Provides the latest weekly RDO update\n**/gta**\n> Provides the latest GTA Online weekly update`)
-    
-		return interaction.reply({ embeds: [helpEmbed] });
+      .setDescription(`**/help**\n> Responds with a list of commands \n**/ping**\n> Responds with pong! when online\n**/code**\n> Responds with a link to the source code of this bot \n**/vote**\n> Responds with a link to vote for this bot on Top.gg\n**/rdo**\n> Provides the latest weekly RDO update\n**/gta**\n> Provides the latest GTA Online weekly update \n\nStill have questions? Join the support server: [Click Here](https://discord.com/invite/TkrFcwHWfj)`)
+
+    await interaction.deferReply();
+		await interaction.editReply({ embeds: [helpEmbed] });
 	},
 };
