f00323cc3d4285c0781e1b525f3e190cc1867a60
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2022-08-30 18:19:16 -0600

    minor bug fix

--- commands/GTA.js
+++ commands/GTA.js
@@ -11,26 +11,14 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.deferReply().catch(console.error);
 
-gtaURL = process.env.SOCIAL_URL_GTA 
-var j = request.jar(); //Allows the gtaURL to remember the username and password
-request = request.defaults({ jar: j }); // signs in to gtaURL
-	request({ // logs in to gtaURL
-		url: gtaURL,
-		method: "GET",
-		form: { UNENTRY: process.env.SOCIAL_USER, PWENTRY: process.env.SOCIAL_PASSWORD } //Provides Username and Password
-	},
-		function(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				//console.log('body1:', body); 
-
-//----------BEGIN Phantom Function to read and format html--------//
-	(async function() {
+		let gtaURL = process.env.SOCIAL_URL_GTA;
 		const instance = await phantom.create();
 		const page = await instance.createPage();
 
 		await page.property('viewportSize', { width: 1024, height: 600 });
 		const status = await page.open(gtaURL);
 			//console.log(`Page opened with status [${status}].`);
+		if (status === `success`) { //checks if Rockstar Social Club website is down
 		const content = await page.property('content'); 
 			//console.log(content); 
 		let gtaString = content.toString(); // Gets the latest gta updates
@@ -39,15 +27,16 @@ request = request.defaults({ jar: j }); // signs in to gtaURL
 			//console.log(`${gtaDate01[1]}`);
 		let gtaDate = gtaDate01[1].split("<"); //cuts off the end of the date
 			//console.log(`Date: ${gtaDate[0]}\n`);
-		let gtaTitles01 = gtaString.split("<p><b>"); //gets the titles
+		let gtaRemoveHTMLTitles = gtaString.replace(/<b>Sprunk/g, "Sprunk "); //removes bold items that confuse the bonus split
+		let gtaTitles01 = gtaRemoveHTMLTitles.split("<p><b>"); //gets the titles
 			//console.log(`Titles: ${gtaTitles01}\n`);			
 		let gtaRemoveHTML = gtaString.replace(/<\/b><\/p><\/ul>/g, "</p>"); //removes bold items that confuse the bonus split
 		let gtaBonuses01 = gtaRemoveHTML.split("</b></p>");	//gets the bonuses
 			//console.log(`gtaBonuses01: \n${gtaBonuses01}\n`);
 		let gtaImage01 = gtaString.split("og:image\" content=\"");
-			//console.log(`${gtaImage001[1]}`);
-		let gtaImage = gtaImage01[1].split("\" data-rh=\"\">");
-			//console.log(`${gtaImage[0]}`);		
+			//console.log(`gtaImage01: ${gtaImage01[1]}`);
+		let gtaImage = gtaImage01[1].split("\" data-rh=");
+			//console.log(`gtaImage: ${gtaImage[0]}`);		
 		// Strings to be populated with the indexes of the two titles after the "Only on PlayStation..." title
 		let nextGenIndex1 = ""; // empty string to be populated
 		let nextGenIndex2 = ""; // empty string to be populated
@@ -105,11 +94,11 @@ request = request.defaults({ jar: j }); // signs in to gtaURL
 						//console.log(`Titles2 at J: ${j}: ${Titles2[j]}\n`); //checks for blank elements
 						//console.log(`${Titles2[j].charAt(0)}${Titles2[j].toLowerCase().slice(1)}`); //capital first letters check 
 	//returns first letter capitalized + rest of the word lowercase if the word is the first word in the title - unless GTA
-					if ( (Titles2[j] === Titles2[0]) && (!Titles2[j].includes("GTA")) ) { 
+					if ( (Titles2[j] === Titles2[0]) && (!Titles2[j].includes("GTA")) && (Titles2[j] != "XP") && (Titles2[j] != "RP") && (Titles2[j] != "GT") && (Titles2[j] != "LD") && (Titles2[j] != "LSPD") ) { 
 						gtaTitleString += `${Titles2[j].charAt(0)}${Titles2[j].toLowerCase().slice(1)} `; 
 					}
 	//returns all caps if title is GTA, GTA$, or XP							
-					else if ( (Titles2[j].includes("GTA")) || (Titles2[j] === "XP") || (Titles2[j] === "RP") || (Titles2[j] === "GT")  ) { 
+					else if ( (Titles2[j].includes("GTA")) || (Titles2[j] === "XP") || (Titles2[j] === "RP") || (Titles2[j] === "GT")  || (Titles2[j] === "LD") || (Titles2[j] === "LSPD") ) { 
 							gtaTitleString += `${Titles2[j]} `;
 					}
 	//returns all lowercase if not a title word					
@@ -156,19 +145,19 @@ request = request.defaults({ jar: j }); // signs in to gtaURL
 				if (gtaParaCount >= 3) { // if more than one paragraph, returns the second paragraph
 						//console.log(`2: gtaParas at I - ${i}: ${gtaParas[1]}\n`);
 					if ( (gtaParas[1] != null) && (gtaParas[2] != null)) {
-						gtaFinalString += `**${gtaTitles}**\n • ${gtaParas[1]}• ${gtaParas[2]}`; 
+						gtaFinalString += `**${gtaTitles}**\n • ${gtaParas[1]} • ${gtaParas[2]}`; 
 					} 	
 					if ( (gtaParas[1] != null) && (gtaParas[2] === null)) {
 						gtaFinalString += `**${gtaTitles}**\n • ${gtaParas[1]}`; 
 					} 					
 				} else if (gtaParaCount <= 2) { // if one or 0 paragraphs, returns only the title - unless "Luxury Autos Showroom...""
-					if (gtaParas[0].includes("Luxury Autos Showroom")) { 
+					if (gtaParas[1].toLowerCase().includes("luxury autos showroom")) { 
 							//console.log(`3: gtaParas at I - ${i}: ${gtaParas[1]}\n`);
-						gtaFinalString += `**${gtaTitles}**\n • ${gtaParas[0]}`; 
+						gtaFinalString += `**${gtaTitles}**\n • ${gtaParas[1]}`; 
 					} 
 					//-----END paragraph count-----//	
-					else { 
-							//console.log(`4: Only Title at ${i}: ${gtaTitles}`);
+					else { 					
+							//console.log(`4: Only Title at ${i}: ${gtaTitles} para: ${gtaParas[1]}`);
 						gtaFinalString += `**${gtaTitles}**\n\n\n\n\n\n`;
 					}
 				}
@@ -203,7 +192,7 @@ request = request.defaults({ jar: j }); // signs in to gtaURL
 			else if ( ( (gtaBonuses[0].includes("style=")) || (gtaBonuses[0].includes("<li>")) ) && (!gtaBonuses[0].includes("<a")) && (!gtaBonuses[0].includes("body>"))) {
 				let gtaBonuses05 = gtaBonuses[0].split("style=\"line-height:1.5;\">");
 					//console.log(`8: Bonuses05 at ${i}: \n${gtaTitles}\n${gtaBonuses05[1]}\n\n`);
-				if (gtaBonuses05[0].includes("Luxury Autos")) {
+				if (gtaBonuses05[0].toLowerCase().includes("luxury autos")) {
 					gtaFinalString += `**${gtaTitles}** ${gtaBonuses05[0]}`;
 				}
 				else if (gtaBonuses05[1] != null) {
@@ -221,10 +210,46 @@ request = request.defaults({ jar: j }); // signs in to gtaURL
 
 //---------------------------END populating gtaFinalString-------------------------//
 
+
+    function gtaPost() {
+        return gtaFinalString.slice(0, 4097);
+    }
+    //console.log(`1: ${gtaFinalString.length}\n`) 
+    function gtaPost2() {
+      if (gtaFinalString.length > 4096) {
+        let post02 = gtaFinalString.substr(4097, 2300);
+        return post02;
+      } else {
+        return "";
+      }
+    }  
+    function elipseFunction() {
+      if (gtaFinalString.length > 4096) {
+        return "...";
+        } else {
+        return "";
+        }
+    }		
+    function gtaFooterMax() {
+      if (gtaFinalString.length > 4096) {
+        return `** [click here](${gtaURL}) for more details**`;
+      } else {
+        return "";
+      }
+    }
+    function gtaFooterMin() { 
+      if (gtaFinalString.length <= 4096) {
+        return `** [click here](${gtaURL}) for more details**`;
+      } else {
+        return "";
+      }
+    }  
+
+		
 		let gtaEmbed = new MessageEmbed()
 			.setColor('#00CD06') //Green
 			.setTitle('Grand Theft Auto V Online Weekly Bonuses & Discounts:')
-			.setDescription(`${gtaDate[0]}\n\n${gtaFinalString}\n\n\n\n\n**[click here](${gtaURL}) for more details**`
+			.setDescription(`${gtaDate[0]}\n\n${gtaPost()}\n\n\n\n\n${gtaFooterMin()} ${elipseFunction()}`
 				.replace(/\s\s\s\s/g, '') //removes multiple spaces
 				.replace(/<li>/g, "• ")
 				.replace(/<p>/g, "• ")
@@ -242,12 +267,38 @@ request = request.defaults({ jar: j }); // signs in to gtaURL
 				.replace(/• •/g, "• ") //removes multiple bullet points
 				.replace(/GTA\+ website/g, "[GTA+ website](https://www.rockstargames.com/gta-plus)")
 				.replace(/GTA\+ Guide/g, "[GTA+ Guide](https://www.rockstargames.com/gta-online/guides/7539)")
-				.replace(/Prime Gaming and sign up/g, "[Prime Gaming](https://gaming.amazon.com/loot/gtaonline)")
+				.replace(/ Prime Gaming/g, " [Prime Gaming](https://gaming.amazon.com/loot/gtaonline)")
+				.replace(/Rockstar Games Social Club/g, "[Rockstar Games Social Club](https://socialclub.rockstargames.com)")
 				.replace(/Check Rockstar Support/g, "[Check Rockstar Support](https://support.rockstargames.com/articles/360053937434/Claiming-Rockstar-Games-Social-Club-x-span-class-highlight-Prime-Gaming-span-benefits)")
 			)
+		let gtaEmbed2 = new MessageEmbed()
+			.setColor('#00CD06') //Green
+			.setDescription(`${elipseFunction()} ${gtaPost2()}\n\n\n\n\n${gtaFooterMax()}`
+				.replace(/\s\s\s\s/g, '') //removes multiple spaces
+				.replace(/<li>/g, "• ")
+				.replace(/<p>/g, "• ")
+				.replace(/<b>/g, "")
+				.replace(/<\/li>/g, "")
+				.replace(/<\/p>/g, "")
+				.replace(/<\/b>/g, "")
+				.replace(/<ul /g, "")
+				.replace(/<\/ul>/g, "")
+				.replace(/<\/a>/g, "")
+				.replace(/&amp;/g, "&") //Ampersand						
+				.replace(/&nbsp;/g, " ") //Non breaking space
+	// Below this differs from RDO 	
+				.replace(/\n\n• /g, "\n• ") //removes double spaces before a list
+				.replace(/• •/g, "• ") //removes multiple bullet points
+				.replace(/GTA\+ website/g, "[GTA+ website](https://www.rockstargames.com/gta-plus)")
+				.replace(/GTA\+ Guide/g, "[GTA+ Guide](https://www.rockstargames.com/gta-online/guides/7539)")
+				.replace(/ Prime Gaming/g, " [Prime Gaming](https://gaming.amazon.com/loot/gtaonline)")
+				.replace(/Rockstar Games Social Club/g, "[Rockstar Games Social Club](https://socialclub.rockstargames.com)")
+				.replace(/Check Rockstar Support/g, "[Check Rockstar Support](https://support.rockstargames.com/articles/360053937434/Claiming-Rockstar-Games-Social-Club-x-span-class-highlight-Prime-Gaming-span-benefits)")
+			)		
 		let gtaImageEmbed = new MessageEmbed()
 			.setColor('#00CD06') //Green
-			.setImage(gtaImage[0])
+			.setImage(`${gtaImage[0]}`)
+			//console.log(`${gtaImage[0]}`);
 
 		const aDate = new Date();
 		const aDay = aDate.getDay(); //Day of the Week
@@ -257,22 +308,22 @@ request = request.defaults({ jar: j }); // signs in to gtaURL
 		
  		let gtaExpiredEmbed = new MessageEmbed()
 			.setColor('#00CD06') //Green
-			.setDescription(`The above bonuses & discounts may be expired. \nRockstar updates these bonuses every Thursday after 12:30 PM EST`)
+			.setDescription(`The above bonuses & discounts may be expired. \nRockstar releases the latest weekly bonuses & discounts every \nThursday after 12:30 PM EST`)
 
-		await interaction.editReply({ embeds: [gtaImageEmbed, gtaEmbed] }).catch(console.error);
+		if (gtaFinalString.length <= 4096) {
+			await interaction.editReply({ embeds: [gtaEmbed] }).catch(console.error);
+		} else {
+			await interaction.editReply({ embeds: [gtaEmbed, gtaEmbed2] }).catch(console.error);
+		}
 		//if ( (aDay === 5) ) { //Test for today 0 = Sunday, 1 = Monday ... 6 = Saturday
-		if ( (aDay === 4) && (aHour <= 17) ) { //If it's Thursday(4) before 1:00PM EST (17)
+		if ( (aDay === 4) && (aHour < 17) ) { //If it's Thursday(4) before 1:00PM EST (17)
 			interaction.followUp({embeds: [gtaExpiredEmbed], ephemeral:true}).catch(console.error);
 		}		
-		
-		await instance.exit(); 
-		})()
 
-} else {
-	console.log('error2', error, response && response.statusCode);
-}
-
-});
 		//interaction.editReply(`Console logged! 👍`);
+		} else {
+			interaction.editReply(`The Rockstar Social Club website is down. \nPlease try again later. \nSorry for the inconvenience.`)
+			console.log(`The Rockstar Social Club website is down.`)
+		}
 	}
 }
