b66626d0f45b95a9b0e214e6a9b81bce7c4ef1b5
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2022-09-21 16:13:24 -0600

    Updated to Discord.js v14

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,31 @@
+const { Client, GatewayIntentBits, Collection, Partials } = require('discord.js');
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
+	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+});
+
 module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
-		console.log(`an interaction was successfully triggered`);
+		//console.log(`interaction.channel.type: ${interaction.channel.type}`);
+		if (interaction.channel.type === 0) { 
+			if ((interaction.user.id === process.env.USER_ID_1) || (interaction.user.id === process.env.USER_ID_2)) {
+				console.log(`You triggered ${interaction} in a guild`);
+			} 
+			else {
+				console.log(`A user triggered ${interaction} in a guild`);
+			}
+		}
+		else if (interaction.channel.type === 1) {
+			if ((interaction.user.id === process.env.USER_ID_1) || (interaction.user.id === process.env.USER_ID_2)) {
+				console.log(`You triggered ${interaction} in a DM`);
+			} 
+			else {
+				console.log(`A user triggered ${interaction} in a DM`);
+			}
+		}
+		else {
+			console.log(`A user triggered ${interaction} ...somewhere?`);
+		}
 	},
 };
