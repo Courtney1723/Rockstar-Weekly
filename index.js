51a2c68b89dd6a6d1ef34c7931629ba79616c552
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2022-09-21 16:12:55 -0600

    Updated to Discord.js v14

--- index.js
+++ index.js
@@ -1,40 +1,59 @@
-const fs =  require('node:fs');
-const path = require('node:path');
-const keep_alive = require('./keep_alive.js')
-const { Client, Collection, Intents, MessageEmbed} = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES], partials: ["CHANNEL"] });
-const { exec } = require('node:child_process'); //for the kill 1 command when 429 error is caught.
+ 1
+	 const { Client, GatewayIntentBits, Collection, Partials, EmbedBuilder } = require('discord.js');
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
+	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+});
+const keepAlive = require('./server');
 
 // node deploy-commands.js
 //^^ type in shell to register a command
 
-//Bot rich presence
 client.on("ready", () => {
-    client.user.setPresence({ activities: [{ name: 'Bonuses', type: 'WATCHING'  }], status: 'idle' });  
-});
 
-//Guild Count
-client.on("ready", () => {
-    const Guilds = client.guilds.cache.map(guild => guild.id);
-    console.log(`${Guilds.length} guilds`);
-});
+		//Logs all console logs in Discord - uncomment for main bot
+		console.log = function(log) {
+			if ((log.includes(`guilds`)) || (log.includes(`Logged in`)) || (log.includes(`You triggered`)) ) {
+				const logChannel = client.channels.cache.get(process.env.logChannel2);	
+				let logEmbed = new EmbedBuilder()
+					.setColor('0xFF008B') //Pink
+					.setDescription(`${log}`);
+				logChannel.send({embeds: [logEmbed]});
+			} 
+			else {
+				const logChannel = client.channels.cache.get(process.env.logChannel);	
+				let logEmbed = new EmbedBuilder()
+					.setColor('0xFF008B') //Pink
+					.setDescription(`${log}`);
+				logChannel.send({embeds: [logEmbed]});
+			}
+		}
+	
+})
+
+//prevents unhandled promise rejections from shutting the bot off
+client.on("unhandledRejection", error => console.error("Promise rejection:", error));
 
 //Access Command Files
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
+const fs = require('node:fs');
+const path = require('node:path');
 
+client.commands = new Collection();
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
+	commands.push(command.data.toJSON());
 	client.commands.set(command.data.name, command);
 }
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+	if (!interaction.isChatInputCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+	const command = interaction.client.commands.get(interaction.commandName);
 
 	if (!command) return;
 
@@ -42,14 +61,14 @@ client.on('interactionCreate', async interaction => {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		await interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: true });
+		console.log(`error: ${error}`);
 	}
 });
 
-//Access Event Files
+//Access Event files
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
 
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
@@ -73,9 +92,9 @@ client.on("debug", function(info){
 				        return
 				    }
 				  console.log(`Kill 1 command succeeded`); //probably wont work
-				});			
-		}
+				});	
+		} 
 });
 
-//login
-client.login(process.env['DISCORD_TOKEN']).catch(err => console.log(err));
+keepAlive();
+client.login(process.env.DISCORD_TOKEN).catch(err => console.log(`Login Error: ${err}`));
