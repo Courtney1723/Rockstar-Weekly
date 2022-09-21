01d7f069ca4148e8861e7b52d6129b7011aea3e9
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2022-09-21 16:15:14 -0600

    Updated to Discord.js v14

--- keep_alive.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.write("Logged in as Rockstar Games Weekly Bonuses and Discounts Bot! \nCreated by Courtney1723");
-  res.end();
-}).listen(8080);
