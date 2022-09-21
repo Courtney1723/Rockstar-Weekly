bdf7ec0323808ed123529de87657a4816fccfe53
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2022-09-21 16:16:29 -0600

    Updated to Discord.js v14

--- commands/code.js
+++ commands/code.js
@@ -1,11 +1,12 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('code')
-		.setDescription('Replies with a link to the source code of the Rockstar Weekly bot'),
+		.setDescription('The source code of the Rockstar Weekly bot')
+		.setDMPermission(true),
 	async execute(interaction) {
-		await interaction.deferReply();
-    await interaction.editReply(`Rockstar Weekly source code: [Github Link](https://github.com/Courtney1723/Rockstar-Weekly)`).catch(err => {console.log(err)});
+		await interaction.deferReply().catch(console.error);
+    await interaction.editReply(`Rockstar Weekly source code: [Github Link](<https://github.com/Courtney1723/Rockstar-Weekly>)`).catch(err => {console.log(err)});
 	},
 };
