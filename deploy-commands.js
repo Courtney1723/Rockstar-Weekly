e2692445733fea19f1b551153620897894a53660
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2022-09-21 16:14:21 -0600

    Updated to Discord.js v14

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,35 +1,28 @@
+const { Client, Collection, GatewayIntentBits, REST, Routes } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const fs = require('node:fs');
-const path = require('node:path');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const token = process.env.DISCORD_TOKEN
-
 
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-const  clientId  = process.env.clientId;
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
+	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-    await rest.put(
-      	Routes.applicationCommands(clientId),
-      	{ body: commands },
-      );
+        const data = await rest.put(
+					Routes.applicationCommands(process.env.CLIENT_ID),
+					{ body: commands },
+				);
 
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
 })();
